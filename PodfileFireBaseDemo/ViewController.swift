53c4e4fdcfc9682c77113b0ed535ed57243968a1
admin <chiinhuv@gmail.com>
2019-02-26 14:03:22 +0700

    add pod firebase, log in facebook, google

---
+++ PodfileFireBaseDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PodfileFireBaseDemo
+//
+//  Created by admin on 2/22/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
